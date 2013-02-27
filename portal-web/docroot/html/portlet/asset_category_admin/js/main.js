a8a6eec1b03052e982dd5936aa20768a1003ddf8
Laszlo Csontos <laszlo.csontos@liferay.com>
2013-02-27 15:47:37 +0100

    LPS-33236 Deletion reports success, even though an exception has been thrown by the backend

--- portal-web/docroot/html/portlet/asset_category_admin/js/main.js
+++ portal-web/docroot/html/portlet/asset_category_admin/js/main.js
@@ -2122,10 +2122,14 @@ AUI.add(
 						);
 					},
 
-					_processCategoryDeletion: function(result) {
+					_processCategoryDeletion: function() {
 						var instance = this;
 
-						var exception = result.exception;
+						var exception;
+
+						if (arguments.length > 1) {
+							exception = arguments[0];
+						}
 
 						if (!exception) {
 							instance._closeEditSection();
@@ -2162,15 +2166,21 @@ AUI.add(
 						}
 					},
 
-					_processVocabularyDeletion: function(result) {
+					_processVocabularyDeletion: function() {
 						var instance = this;
 
-						var exception = result.exception;
+						var exception;
+
+						if (arguments.length > 1) {
+							exception = arguments[0];
+						}
 
 						if (!exception) {
 							instance._closeEditSection();
 							instance._hidePanels();
 							instance._loadData();
+
+							instance._sendMessage(MESSAGE_TYPE_SUCCESS, Liferay.Language.get('your-request-processed-successfully'));
 						}
 						else {
 							var errorKey;
