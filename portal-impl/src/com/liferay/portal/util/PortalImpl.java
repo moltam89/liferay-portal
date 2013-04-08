232723ef8f55e49b23cbdcaf75825b9769df6740
Gergely Mathe <gergely.mathe@liferay.com>
2013-04-08 15:59:04 +0200

    LPS-34187 Back link of an asset is not working if "view in context" is used with a virtual host

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6144,7 +6144,7 @@ public class PortalImpl implements Portal {
 		LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
 			group.getGroupId(), privateLayoutSet);
 
-		String portalURL = StringPool.BLANK;
+		String portalURL = themeDisplay.getURLPortal();
 
 		if (canonicalURL || !themeDisplay.getServerName().equals(_LOCALHOST)) {
 			String virtualHostname = layoutSet.getVirtualHostname();
