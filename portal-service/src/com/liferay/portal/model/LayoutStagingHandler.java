4ad8f2e7836a07f30eeab477f539ec0c175b319d
vilmospapp <vilmos.papp@liferay.com>
2013-07-16 13:49:36 +0200

    LPS-37302 Added isContentDisplayPage to _layoutRevisionMehtodNames in the Proxy

--- portal-service/src/com/liferay/portal/model/LayoutStagingHandler.java
+++ portal-service/src/com/liferay/portal/model/LayoutStagingHandler.java
@@ -289,6 +289,7 @@ public class LayoutStagingHandler implements InvocationHandler, Serializable {
 		_layoutRevisionMethodNames.add("getWapColorSchemeId");
 		_layoutRevisionMethodNames.add("getWapTheme");
 		_layoutRevisionMethodNames.add("getWapThemeId");
+		_layoutRevisionMethodNames.add("isContentDisplayPage");
 		_layoutRevisionMethodNames.add("isEscapedModel");
 		_layoutRevisionMethodNames.add("isIconImage");
 		_layoutRevisionMethodNames.add("isInheritLookAndFeel");
