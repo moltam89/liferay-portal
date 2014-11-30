ccc2cb528c30c60828fbd89ee66ff3b8aea2c7ac
Tamas Molnar <tamas.molnar@liferay.com>
2014-11-30 12:03:27 +0100

    Do not export/import JournalPortlet for testing

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandler.java
@@ -170,6 +170,10 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 		rootElement.addAttribute(
 			"group-id", String.valueOf(portletDataContext.getScopeGroupId()));
 
+		if (true) {
+			return getExportDataRootElementString(rootElement);
+		}
+
 		if (portletDataContext.getBooleanParameter(NAMESPACE, "feeds")) {
 			ActionableDynamicQuery feedActionableDynamicQuery =
 				JournalFeedLocalServiceUtil.getExportActionableDynamicQuery(
@@ -223,6 +227,10 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 			PortletDataContext portletDataContext, String portletId,
 			PortletPreferences portletPreferences, String data)
 		throws Exception {
+
+		if (true) {
+			return portletPreferences;
+		}
 
 		portletDataContext.importPortletPermissions(
 			JournalPermission.RESOURCE_NAME);
