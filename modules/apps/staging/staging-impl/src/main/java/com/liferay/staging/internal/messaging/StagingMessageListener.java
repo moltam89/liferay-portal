27651b9746a9ae6fdecfd473e8e30fbe95524bd7
Kimberly Chau <kimberly.chau@liferay.com>
2019-06-10 09:31:17 -0700

    LPS-94690 Implement background thread to monitor the remote server as well as update the remote display URL, if there is some connection error, the remote URL will be set to null
    https://issues.liferay.com/browse/LPS-94690

--- modules/apps/staging/staging-impl/src/main/java/com/liferay/staging/internal/messaging/StagingMessageListener.java
+++ modules/apps/staging/staging-impl/src/main/java/com/liferay/staging/internal/messaging/StagingMessageListener.java
@@ -14,9 +14,17 @@
 
 package com.liferay.staging.internal.messaging;
 
+import com.liferay.exportimport.kernel.staging.Staging;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
@@ -24,19 +32,28 @@ import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.util.PropsValues;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
+import java.net.ConnectException;
+import java.util.Map;
+
 /**
  * @author Kimberly Chau
  */
 @Component(
-	immediate = true,
-	property = "destination.name=" + DestinationNames.STAGING,
+	immediate = true, property = "destination.name=" + DestinationNames.STAGING,
 	service = StagingMessageListener.class
 )
 public class StagingMessageListener extends BaseMessageListener {
@@ -68,8 +85,73 @@ public class StagingMessageListener extends BaseMessageListener {
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
+		String cachedRemoteURL;
+
+		Map<String, Object> messageValues = message.getValues();
+
+		if (messageValues.get("group") != null) {
+			_group = (Group)messageValues.get("group");
+
+			if (_group.isStaged() || _group.isStagedRemotely()) {
+				_typeSettingsProperties = _group.getTypeSettingsProperties();
+
+				_liveGroupId = GetterUtil.getLong(
+					_typeSettingsProperties.getProperty("remoteGroupId"));
+			}
+			else {
+				_liveGroupId = 0;
+			}
+		}
+
+		if (_liveGroupId > 0) {
+			if (messageValues.get("layout") != null) {
+				_layout = (Layout) messageValues.get("layout");
+			}
+
+			if (messageValues.get("user") != null) {
+				user = (User) messageValues.get("user");
+			}
+
+			initThreadLocals(user.getUserId());
+
+			try {
+				cachedRemoteURL =
+					_staging.getRemoteSiteURL(_group, _layout.isPrivateLayout());
+			}
+			catch (SystemException se) {
+				Throwable cause = se.getCause();
+
+				if (!(cause instanceof ConnectException)) {
+					_log.error(se, se);
+				}
+
+				cachedRemoteURL = null;
+			}
+			_typeSettingsProperties.setProperty("remoteURL", cachedRemoteURL);
+		}
 	}
 
+	protected void initThreadLocals(long userId) throws PortalException {
+		User user = _userLocalService.getUserById(userId);
+
+		CompanyThreadLocal.setCompanyId(user.getCompanyId());
+
+		PrincipalThreadLocal.setName(userId);
+
+		PermissionChecker permissionChecker = null;
+
+		try {
+			permissionChecker = PermissionCheckerFactoryUtil.create(user);
+		}
+		catch (Exception e) {
+			throw new SystemException(
+				"Unable to initialize thread locals because an error occured " +
+				"when creating a permission checker for user " + userId,
+				e);
+		}
+
+		PermissionThreadLocal.setPermissionChecker(permissionChecker);
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
@@ -83,9 +165,23 @@ public class StagingMessageListener extends BaseMessageListener {
 		_schedulerEngineHelper = schedulerEngineHelper;
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		StagingMessageListener.class);
+
+	private Group _group;
+	private Layout _layout;
+	private long _liveGroupId;
 	private SchedulerEngineHelper _schedulerEngineHelper;
+	private UnicodeProperties _typeSettingsProperties;
+	private User user;
+
+	@Reference
+	private Staging _staging;
 
 	@Reference
 	private TriggerFactory _triggerFactory;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
