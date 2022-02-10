9b6d412eecd2b772589c1d6d6cf640cddfe54e86
tamas.molnar <tamas.molnar@liferay.com>
2022-02-10 11:38:42 +0100

    LPS-144106 Get Stagingconfig

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/CookieImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/CookieImpl.java
@@ -1,14 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.exportimport.internal.staging;
+
+import com.liferay.cookie.util.Cookie;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.staging.configuration.StagingConfiguration;
+
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 
-import com.liferay.cookie.util.Cookie;
-
-
-@Component(immediate = true, service = Cookie.class)
+/**
+ * @author Tamas Molnar
+ */
+@Component(
+	configurationPid = "com.liferay.staging.configuration.StagingConfiguration",
+	immediate = true, service = Cookie.class
+)
 public class CookieImpl implements Cookie {
 
 	@Override
@@ -19,4 +44,14 @@ public class CookieImpl implements Cookie {
 
 		httpServletResponse.addCookie(cookie);
 	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_stagingConfiguration = ConfigurableUtil.createConfigurable(
+			StagingConfiguration.class, properties);
+	}
+
+	private volatile StagingConfiguration _stagingConfiguration;
+
 }
