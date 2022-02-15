67d0f7808ce6396e0f11982bb4f54037e7269390
tamas.molnar <tamas.molnar@liferay.com>
2022-02-15 14:59:04 +0100

    LPS-148284 Use CookieConsent in CookieKeys

--- portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
@@ -95,6 +95,12 @@ public class CookieKeys {
 			return;
 		}
 
+		if (!CookieConsentUtil.isCookieAllowed(
+				httpServletRequest, cookie, type)) {
+
+			return;
+		}
+
 		// LEP-5175
 
 		String name = cookie.getName();
