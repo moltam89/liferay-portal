3f58a27bd50c1ec60f95c355eae14fb963938ce6
tamas.molnar <tamas.molnar@liferay.com>
2022-02-17 11:19:41 +0100

    LPS-135623 Add @ProviderType

--- portal-kernel/src/com/liferay/portal/kernel/util/CookieConsent.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/CookieConsent.java
@@ -2,6 +2,9 @@ package com.liferay.portal.kernel.util;
 
 import javax.servlet.http.Cookie;
 
+import org.osgi.annotation.versioning.ProviderType;
+
+@ProviderType
 public interface CookieConsent {
 
 	public boolean isCookieAllowed(Cookie cookie, String type);
