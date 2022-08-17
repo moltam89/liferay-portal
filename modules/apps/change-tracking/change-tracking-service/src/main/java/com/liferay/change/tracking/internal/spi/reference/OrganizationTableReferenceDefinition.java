5264b20ebdcdbdf05e0e326d8615909c8f1394fc
tamas.molnar <tamas.molnar@liferay.com>
2022-08-17 13:40:13 +0200

    LPS-161618 Map EmailAddress in OrganizationTableReferenceDefinition

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/reference/OrganizationTableReferenceDefinition.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/reference/OrganizationTableReferenceDefinition.java
@@ -20,6 +20,7 @@ import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInf
 import com.liferay.portal.kernel.model.ClassNameTable;
 import com.liferay.portal.kernel.model.CompanyTable;
 import com.liferay.portal.kernel.model.CountryTable;
+import com.liferay.portal.kernel.model.EmailAddressTable;
 import com.liferay.portal.kernel.model.GroupTable;
 import com.liferay.portal.kernel.model.ImageTable;
 import com.liferay.portal.kernel.model.ListTypeConstants;
@@ -77,6 +78,9 @@ public class OrganizationTableReferenceDefinition
 			OrganizationTable.INSTANCE.logoId, ImageTable.INSTANCE.imageId
 		).assetEntryReference(
 			OrganizationTable.INSTANCE.organizationId, Organization.class
+		).classNameReference(
+			OrganizationTable.INSTANCE.organizationId,
+			EmailAddressTable.INSTANCE.classPK, Organization.class
 		).resourcePermissionReference(
 			OrganizationTable.INSTANCE.organizationId, Organization.class
 		).systemEventReference(
