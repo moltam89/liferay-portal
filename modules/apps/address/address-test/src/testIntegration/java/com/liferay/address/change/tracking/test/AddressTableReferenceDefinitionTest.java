7a9d3b91cd4fd6d1f07ffe6d845d6949a4029b17
tamas.molnar <tamas.molnar@liferay.com>
2022-08-18 10:15:58 +0200

    LPS-132609 Create AddressTableReferenceDefinitionTest

---
+++ modules/apps/address/address-test/src/testIntegration/java/com/liferay/address/change/tracking/test/AddressTableReferenceDefinitionTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.address.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.ListType;
+import com.liferay.portal.kernel.model.ListTypeConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.service.AddressLocalService;
+import com.liferay.portal.kernel.service.ListTypeLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Tamas Molnar
+ */
+@RunWith(Arquillian.class)
+public class AddressTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		User user = TestPropsValues.getUser();
+
+		ListType listType = _listTypeLocalService.getListType(
+			"personal", ListTypeConstants.CONTACT_ADDRESS);
+
+		return _addressLocalService.addAddress(
+			null, user.getUserId(), Contact.class.getName(),
+			user.getContactId(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), null,
+			null, RandomTestUtil.randomString(), null, 0, 0,
+			listType.getListTypeId(), false, false, "1234567890",
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	@Inject
+	private static AddressLocalService _addressLocalService;
+
+	@Inject
+	private static ListTypeLocalService _listTypeLocalService;
+
+}
